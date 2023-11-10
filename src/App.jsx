a168ec00f1303865ea2e518c57470e86c15ae133
Jay Leverton <leverton.web@gmail.com>
2023-11-10 04:45:44 +0000

    Moved components to components folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
