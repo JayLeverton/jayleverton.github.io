4a76243fa5e4e121e50e743b357cebc077c86347
Jay Leverton <leverton.web@gmail.com>
2023-11-10 06:10:25 +0000

    think I've finally fixed it? maybe?

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: false,
+  },
   base: '/',
   plugins: [react()],
   server: {
