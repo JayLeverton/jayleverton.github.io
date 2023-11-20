b1073f897a3871a3dc65aac70f79a316d5c1132c
Jay Leverton <leverton.web@gmail.com>
2023-11-20 14:46:51 +0000

    BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import "./index.css";
 import React, { StrictMode } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
