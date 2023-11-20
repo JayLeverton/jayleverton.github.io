1aa5c1dbba03636b41914b79820455a700246b65
Jay Leverton <leverton.web@gmail.com>
2023-11-20 15:08:08 +0000

    gh pages experimentation. Reverted changes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import "./index.css";
 import React, { StrictMode } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  // <StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-  /* </StrictMode> */
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
