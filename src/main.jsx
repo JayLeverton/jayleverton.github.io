723c1701da7d033044e27426837caad27db8ed99
Jay Leverton <leverton.web@gmail.com>
2023-11-20 14:51:08 +0000

    experimenting with strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import App from "./App.jsx";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+  // <StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
+  /* </StrictMode> */
 );
